1d37d1920e5a62bdb198f11493932510f9dd534d
Michael Douglas <michael.dcsouza@gmail.com>
2019-01-10 00:33:50 -0200

    -

--- config/firebase-storage.js
+++ config/firebase-storage.js
@@ -1,19 +1,16 @@
 //config/firebase-storage.js
 
 //Firebase Admin
-var firebase = require("firebase-admin");
+var admin = require("firebase-admin");
 
-var config = {
-  apiKey: "AIzaSyC8FNp-fywFyjYt3Y43VCVOf8hRcEfRALk",
-  authDomain: "confidant-api-228201.firebaseapp.com",
-  databaseURL: "https://confidant-api-228201.firebaseio.com",
-  projectId: "confidant-api-228201",
-  storageBucket: "confidant-api-228201.appspot.com",
-  messagingSenderId: "963343962205"
-};
-firebase.initializeApp(config);
+var serviceAccount = require("./confidant-api-228201-firebase-adminsdk-rmmr5-f775beb4b8.json");
 
-var bucket = firebase.storage().bucket();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "https://confidant-api-228201.firebaseio.com"
+});
+
+var bucket = admin.storage().bucket();
 
 module.exports = {
     bucket: bucket
